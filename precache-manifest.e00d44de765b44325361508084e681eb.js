34a556b7e3e6638dde95c796a1c98fb94c834dc6
dhruvi16 <dhruvibutti9477@gmail.com>
2020-09-10 16:21:28 +0530

    deploy

---
+++ precache-manifest.e00d44de765b44325361508084e681eb.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82afbfaa2a64cebfd0077f73abe15659",
+    "url": "/ipl-dashboard/apple-icon.png"
+  },
+  {
+    "revision": "8d2ae821211658deff92",
+    "url": "/ipl-dashboard/css/app.e6322e5d.css"
+  },
+  {
+    "revision": "d5bcf6bc0d46ceb34c01",
+    "url": "/ipl-dashboard/css/chunk-vendors.99fee036.css"
+  },
+  {
+    "revision": "f03e10d8d93b2bf7ed2d",
+    "url": "/ipl-dashboard/css/dashboard.2ee1cae5.css"
+  },
+  {
+    "revision": "996d8248f580f8e26e6c45c67da9b5a6",
+    "url": "/ipl-dashboard/favicon.png"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/ipl-dashboard/fonts/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/ipl-dashboard/fonts/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/ipl-dashboard/fonts/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/ipl-dashboard/fonts/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "3b91359dcf1cf96c8440a7532ac61dda",
+    "url": "/ipl-dashboard/img/anime3.png"
+  },
+  {
+    "revision": "2a5a031c63d290c6cd3803e21d8c3b5d",
+    "url": "/ipl-dashboard/img/anime6.png"
+  },
+  {
+    "revision": "d2caaad36a395104bd99f30487798d79",
+    "url": "/ipl-dashboard/img/apple-icon.png"
+  },
+  {
+    "revision": "9b0c49b657dd738a77a37e7dabcc0b5d",
+    "url": "/ipl-dashboard/img/bg5.jpg"
+  },
+  {
+    "revision": "d25412bcd9af1fb98017c8144a4e40d1",
+    "url": "/ipl-dashboard/img/default-avatar.png"
+  },
+  {
+    "revision": "fadc3f3a4c94b151d8b8ed33456c2344",
+    "url": "/ipl-dashboard/img/emilyz.jpg"
+  },
+  {
+    "revision": "4422794dfa779751326660ae681b082e",
+    "url": "/ipl-dashboard/img/favicon.png"
+  },
+  {
+    "revision": "f220d1815d421bfa192ee352780ba194",
+    "url": "/ipl-dashboard/img/img_3115.jpg"
+  },
+  {
+    "revision": "40fdb6e1e72e21ff904da682c46d0116",
+    "url": "/ipl-dashboard/img/james.jpg"
+  },
+  {
+    "revision": "93f1328bd264f02aacef8b0c6b498445",
+    "url": "/ipl-dashboard/img/mike.jpg"
+  },
+  {
+    "revision": "caec81de667b4c49653f6cca14f924a2",
+    "url": "/ipl-dashboard/index.html"
+  },
+  {
+    "revision": "8d2ae821211658deff92",
+    "url": "/ipl-dashboard/js/app.dfaaf697.js"
+  },
+  {
+    "revision": "d5bcf6bc0d46ceb34c01",
+    "url": "/ipl-dashboard/js/chunk-vendors.597e4fd9.js"
+  },
+  {
+    "revision": "d9d4186ab560693187b8",
+    "url": "/ipl-dashboard/js/common.5eae5df8.js"
+  },
+  {
+    "revision": "e8cb5e8bd9e9cca473fe",
+    "url": "/ipl-dashboard/js/common~dashboard.35f3dd9b.js"
+  },
+  {
+    "revision": "f03e10d8d93b2bf7ed2d",
+    "url": "/ipl-dashboard/js/dashboard.a8620511.js"
+  },
+  {
+    "revision": "76f514046bb49125a8eab9644dc83662",
+    "url": "/ipl-dashboard/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ipl-dashboard/robots.txt"
+  }
+]);
